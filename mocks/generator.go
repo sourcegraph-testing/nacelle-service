c61f76ec718c9d58d6c6fa6568766f95b59b9426
Eric Fritz <eric@eric-fritz.com>
2019-06-09 21:41:29 -0500

    Generate mocks.

--- mocks/generator.go
+++ mocks/generator.go
@@ -1,3 +1,3 @@
 package mocks
 
-//go:generate go-mockgen -f github.com/go-nacelle/service -i ServiceContainer service_contatiner.go
+//go:generate go-mockgen -f github.com/go-nacelle/service -i ServiceContainer -o service_container.go
